4e5667fec6b0eaead0f517d85cb55743b33d676b
EliaTarasoff <elia.tarasoff@gmail.com>
2023-08-25 09:54:30 -0600

    chore: add max-vals for Schlage

---
+++ main.go
@@ -0,0 +1,5 @@
+package master_keys
+
+func main() {
+	print("TODO: a real master-key tool")
+}
---
+++ schlage/schlage.go
@@ -0,0 +1,7 @@
+package schlage
+
+const (
+	MAXIMUM_ADJACENT_CUT_SPECIFICATION = 7
+	MAXIMUM_CUT_DEPTH                  = 9
+	MINIMUM_CUT_DEPTH                  = 0
+)
